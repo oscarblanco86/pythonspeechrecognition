86f9b6e70a51c749ecc572a05e8d7cc589cba19c
Oscar <oscarmb86@hotmail.com>
2024-02-19 21:34:41 -0600

    funcional utilizando los comandos agregados en las primeras lineas

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import speech_recognition
-import pyttsx3
+# requiers miniconda
+# https://docs.anaconda.com/free/miniconda/
+# sudo apt-get install portaudio19-dev python3-all-dev python3-all-dev && sudo pip3 install pyaudio   
+import speech_recognition as sr
 
-recognizer = speech_recognition.Recognizer()
-
-while True:
-    try:
-        with speech_recognition.Microphone() as mic:
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio = recognizer.listen(mic)
-
-            text = recognizer.recognize_google(audio)
-            text = text.lower()
-
-            print(f"Recognized {text}")
-
-    except speech_recognition.UnkownValueError():
-        recognizer = speech_recognition.Recognizer()
-        continue
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+ 
+# Speech recognition using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("You said: " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
